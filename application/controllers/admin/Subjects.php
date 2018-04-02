205d24d02bcfc39a101d9a9faff81bed0dd43cba
lexmagnus88 <2019314@student.uma.pt>
2018-04-02 15:54:23 +0100

    Add subjects

--- application/controllers/admin/Subjects.php
+++ application/controllers/admin/Subjects.php
@@ -11,7 +11,39 @@ class Subjects extends CI_Controller {
         }
 
         public function add(){
-		$this->template->load('admin', 'default', 'subjects/add');
+            //Define rules -----------------Name field, readeble name, rules
+            $this->form_validation->set_rules('name', 'Name', 'trim|required|min_length[3]');
+
+            //check state of form validation
+            if($this->form_validation->run() == False){
+                $this->template->load('admin', 'default', 'subjects/add');
+            }else{
+                //create POST array
+                $data = array(
+                    'name' => $this->input->post('name')
+                );
+
+                //insert subject
+                $this->Subject_model->add($data);
+
+                //Activity Array
+                $data = array(
+                    'resource_id' => $this->db->insert_id(),
+                    'type' => 'subject',
+                    'action' => 'added',
+                    'user_id' => 1,
+                    'message' => 'A new sunject was added ('.$data["name"].')'
+                );
+
+                //insert Activity
+                $this->Activity_model->add($data);
+
+                //set message
+                $this->session->set_flashdata('success', 'Subject has been added');
+
+                //redirect
+                redirect('admin/subjects');
+            }
         }
 
         public function edit(){
